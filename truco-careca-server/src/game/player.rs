197b8dff696be416925e5e17937b06048d2749b6
Tiago Chaves <tchaves74@gmail.com>
2023-04-24 17:37:07 -0300

    Refactor fns and vars permissions in game & player

--- truco-careca-server/src/game/player.rs
+++ truco-careca-server/src/game/player.rs
@@ -1,11 +1,20 @@
 #[derive(Debug)]
 pub struct Player {
-  pub name: String,
-  pub is_ready: bool
+    name: String,
+    is_ready: bool,
 }
 
 impl Player {
-  pub fn new(name: String, is_ready: bool) -> Self { 
-    Self { name, is_ready }
-  }
+    pub fn new(name: String, is_ready: bool) -> Self {
+        Self { name, is_ready }
+    }
+
+    pub fn update(&mut self, name: String, is_ready: bool) {
+        self.name = name;
+        self.is_ready = is_ready;
+    }
+
+    pub fn is_ready(&self) -> bool {
+        self.is_ready
+    }
 }
--- truco-careca-server/src/game/states/waiting_for_ready_state.rs
+++ truco-careca-server/src/game/states/waiting_for_ready_state.rs
@@ -1,32 +1,31 @@
-use crate::game::Game;
 use crate::game::game_event::GameEvent;
 use crate::game::state_machine::GameState;
 use crate::game::states::game_started_state::GameStartedState;
+use crate::game::Game;
 
-pub struct WaitingForReadyState {
-}
+pub struct WaitingForReadyState {}
 
 impl WaitingForReadyState {
-  pub fn new() -> Self {
-    println!("WAITING FOR READY STATE");
-    Self {}
-  }
+    pub fn new() -> Self {
+        println!("WAITING FOR READY STATE");
+        Self {}
+    }
 }
 
 impl GameState for WaitingForReadyState {
-  fn update(&self, game: &mut Game, event: GameEvent) -> Option<Box<dyn GameState>> {
-    match event {
-      GameEvent::PlayerReady(id, name) => {
-        let mut player = game.get_player_mut(id);
-        player.name = name;
-        player.is_ready = true;
-      }
-      _ => { }
+    fn update(&self, game: &mut Game, event: GameEvent) -> Option<Box<dyn GameState>> {
+        match event {
+            GameEvent::PlayerReady(id, name) => {
+                if let Some(player) = game.get_player_mut(id) {
+                    player.update(name, true)
+                }
+            }
+            _ => {}
+        }
+        // Verifica se todos os player estao ready
+        if game.are_players_ready() {
+            return Some(Box::new(GameStartedState::new()));
+        }
+        None
     }
-    // Verifica se todos os player estao ready
-    if game.get_players().values().all(|(_, player)| player.is_ready) {
-      return Some(Box::new(GameStartedState::new())); 
-    }
-    None
-  }
 }
